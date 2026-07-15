cae6e76b5d48dd5d8adc4ebf7c714b40341434e0
Jesús Cortacero <hola@jesuscorta.es>
2026-07-15 10:27:32 +0200

    feature: añadir Google Tag Manager GTM-585RLCD

--- inc/assets.php
+++ inc/assets.php
@@ -184,3 +184,36 @@ function mauswp_enqueue_editor_assets(): void {
 	}
 }
 add_action( 'enqueue_block_editor_assets', 'mauswp_enqueue_editor_assets' );
+
+/**
+ * Google Tag Manager container ID.
+ */
+if ( ! defined( 'MAUSWP_GTM_ID' ) ) {
+	define( 'MAUSWP_GTM_ID', 'GTM-585RLCD' );
+}
+
+/**
+ * Output GTM script in <head>.
+ */
+function mauswp_gtm_head(): void {
+	if ( ! defined( 'MAUSWP_GTM_ID' ) || '' === MAUSWP_GTM_ID ) {
+		return;
+	}
+	?>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','<?php echo esc_js( MAUSWP_GTM_ID ); ?>');</script>
+	<?php
+}
+add_action( 'wp_head', 'mauswp_gtm_head', 1 );
+
+/**
+ * Output GTM noscript iframe after <body>.
+ */
+function mauswp_gtm_body_open(): void {
+	if ( ! defined( 'MAUSWP_GTM_ID' ) || '' === MAUSWP_GTM_ID ) {
+		return;
+	}
+	?>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo esc_attr( MAUSWP_GTM_ID ); ?>" height="0" width="0" style="display:none;visibility:hidden" aria-hidden="true"></iframe></noscript>
+	<?php
+}
+add_action( 'wp_body_open', 'mauswp_gtm_body_open', 1 );
